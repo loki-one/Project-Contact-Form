ab62c2f799f24be7115865e0e76a80f5945fe398
Lokesh Jadhav <lokesh.jadhav10@gmail.com>
2016-11-04 14:52:46 +0100

    Code for Form added
    Code for Form added using HTML and Bootstrap4. The contact form display
    Email, Subject and Content fields and a submit button.

--- projectContactForm.php
+++ projectContactForm.php
@@ -2,7 +2,6 @@
 
 	#Creating a contact form using php, bootstrap and javascript.
 
-	echo "Hello World";
 
 ?>
 
@@ -19,7 +18,24 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.5/css/bootstrap.min.css" integrity="sha384-AysaV+vQoT3kOAXZkl02PThvDr8HYKPZhNT5h/CXfBThSRXQ6jW5DO2ekP5ViFdi" crossorigin="anonymous">
   </head>
   <body>
-    <h1>Hello, world!</h1>
+  	<div class="container">
+	    <h1>Get in Touch!</h1>
+	    <form>
+		  <div class="form-group">
+		    <label for="email">Email address</label>
+		    <input type="email" class="form-control" id="email" placeholder="Enter your email">
+		  </div>
+		  <div class="form-group">
+		    <label for="subject">Subject</label>
+		    <input type="text" class="form-control" id="subject">
+		  </div>
+		  <div class="form-group">
+		    <label for="content">What would you like to ask us?</label>
+		    <textarea class="form-control" id="content" rows="3"></textarea>
+		  </div>
+		  <button type="submit" id="submit" class="btn btn-primary">Submit</button>
+		</form>
+	</div>
 
     <!-- jQuery first, then Tether, then Bootstrap JS. -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js" integrity="sha384-3ceskX3iaEnIogmQchP8opvBy3Mi7Ce34nWjpBIwVTHfGYWQS9jwHDVRnpKKHJg7" crossorigin="anonymous"></script>
